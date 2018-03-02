8f5c3232a9dd9ee0ffbdc284b9508945859125fb
NJIsaf <njisaf@gmail.com>
2018-03-01 23:20:49 -0500

    playing and pausing. this took too long to work out. not all the controls are being exposed by the ref, unsure how to do that still.

---
+++ src/scripts/VideoPlayer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {observer} from 'mobx-react';
+
+@observer
+export default class VideoPlayer extends React.Component {
+
+  static propTypes = {
+    id: PropTypes.string,
+    videoSource: PropTypes.string,
+    captions: PropTypes.string,
+    poster: PropTypes.string
+  }
+
+  constructor(props) {
+    super(props);
+    this.video = [];
+  }
+
+  render() {
+    return (
+      <video ref={el => this.video = el} id={this.props.id} tabIndex="-1" poster={this.props.poster} style={{width: '100%', height: 'auto'}}>
+        <source type="video/mp4" src={this.props.videoSource}/>
+        <track kind="captions" src={this.props.captions}/>
+      </video>
+    )
+  }
+
+}
