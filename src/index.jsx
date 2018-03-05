1ebe50c2316740cfa0383f0f2f26408019d736a0
Paul Arce <paularce@carfax.com>
2018-03-04 22:50:19 -0500

    adding npmignore file; updating index layout

--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {AppContainer, module, render} from 'react-hot-loader';
-import {Provider, observer} from 'mobx-react'
-import {masterStore} from './core/stores/master.store';
-import AblePlayer from './scripts/AblePlayer';
-import LegacyVideoPlayer from './scripts/LegacyVideoPlayer';
-import 'bootstrap'; // bootstrap is required for modals
-
-import MockData from '../content/MockData.json';
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppContainer, module, render } from "react-hot-loader";
+import { Provider, observer } from "mobx-react";
+import { masterStore } from "./core/stores/master.store";
+import AblePlayer from "./scripts/AblePlayer";
+import LegacyVideoPlayer from "./scripts/LegacyVideoPlayer";
+import MockData from "../content/MockData.json";
 
 @observer
 class App extends React.Component {
-
   render() {
     return (
       <Provider store={masterStore}>
-        <div>
+        <div className="container">
           <h1>React Able Player</h1>
-          <AblePlayer
-            id={MockData.id}
-            videoSource={MockData.videoSource}
-            poster={MockData.poster}
-            captions={MockData.caption}
-            />
-          <LegacyVideoPlayer
-            id={MockData.id}
-            videoSource={MockData.videoSource}
-            poster={MockData.poster}
-            captions={MockData.caption}
-            />
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <h2>In Development</h2>
+              <AblePlayer
+                id={MockData.id + "-dev"}
+                videoSource={MockData.videoSource}
+                poster={MockData.poster}
+                captions={MockData.caption}
+              />
+            </div>
+            <div className="col-12 col-md-6">
+              <h2>Legacy</h2>
+              <LegacyVideoPlayer
+                id={MockData.id + "-legacy"}
+                videoSource={MockData.videoSource}
+                poster={MockData.poster}
+                captions={MockData.caption}
+              />
+            </div>
+          </div>
         </div>
       </Provider>
-    )
+    );
   }
-
 }
 
-ReactDOM.render(<AppContainer><App/></AppContainer>, document.getElementById('app'));
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  document.getElementById("app")
+);
 // Hot Module Replacement API
 if (module && module.hot) {
-  module.hot.accept('./app', () => {
-    render(App)
+  module.hot.accept("./app", () => {
+    render(App);
   });
 }
