33d19de066f17985600c0657848e1964dcba6e87
Nassir Isaf <njisaf@gmail.com>
2018-03-27 20:42:05 -0400

    okay, that's some kind of structure. store time

---
+++ src/scripts/AblePlayer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Media from './components/Media';
+import Captions from './components/Captions';
+import Controls from './components/Controls';
+import StatusBar from './components/StatusBar';
+
+import '../../styles/main.scss';
+
+export default class AblePlayer extends React.Component {
+
+  static propTypes = {
+    id: PropTypes.string,
+    source: PropTypes.string,
+    captions: PropTypes.string,
+    poster: PropTypes.string
+  }
+
+  render() {
+    return (
+      <div className="ablePlayer__offscreen">
+        <Media
+          ref={ref => this.media = ref}
+          id={this.props.id}
+          source={this.props.source}
+          poster={this.props.poster}
+        />
+        <Captions
+          captions={this.props.captions}
+        />
+        <Controls />
+        <StatusBar />
+      </div>
+    )
+  }
+}
