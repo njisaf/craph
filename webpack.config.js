2512c17b5ceadac1985f0345301365fd3312be5d
Nassir Isaf <njisaf@gmail.com>
2018-04-08 20:01:33 -0400

    tell yer ass what, let's try parcel. maybe webpack is the problem, start fresh.

--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-//env variable passed in from webpack --env=(dev|stage|prod) build call
-module.exports = function(env) {
-  let _env = env || 'prod';
-  return require(`./webpack.config.${_env}.js`)
-}
